1d0560822d9a936756cd4ec7ad5a9314195337a6
river-ceanne <snvencer@gmail.com>
2019-04-25 12:04:56 -0700

    sort on page load added + titles on new renders

--- class-02/js/page1.js
+++ class-02/js/page1.js
@@ -9,14 +9,6 @@ $.get( '../data/page-1.json', function(data) {
   {
     let newImage = new Image($val.image_url,$val.title,$val.description,$val.keyword,$val.horns);
     imgArr.push(newImage);
-    const imgEl = document.createElement('IMG');
-    const titleEl = document.createElement('cite');
-    const divEl = document.createElement('div');
-    imgEl.setAttribute('src',$val.image_url);
-    titleEl.textContent = $val.title;
-    divEl.appendChild(imgEl);
-    divEl.appendChild(titleEl);
-    $('#photo-template').append(divEl);
   });
 
   onLoad();
